e5c7d27439f3a4ba7d5dfe1d4c89887fff5a7d7b
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-03-31 15:07:26 +0200

    fixed search, added some basic styling and changed migrations slightly to work better for the technologies

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,6 +15,11 @@ Route::get('/', [IndexController::class, "index"]);
 
 Route::get("login", [LoginController::class, "index"])->middleware("guest")->name("login");
 Route::post("login", [LoginController::class, "login"])->middleware("guest");
+
+Route::get("students", [StudentsController::class, "index"])->middleware("auth")->name("students");
+Route::get("students/search", [StudentsController::class, "searchStudentsByTechnologies"])
+    ->middleware("auth")
+    ->name("students.searchByTechnologies");
 
 Route::get('logout', LogoutController::class)->name("logout");
 Route::get('register', [RegisterController::class, "index"])->middleware("guest")->name("register");
