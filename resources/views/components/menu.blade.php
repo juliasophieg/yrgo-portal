a29cd14cba32de4d958dd1c46647426b545c8de9
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-01 17:40:40 +0200

    changed menu around slightly

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -5,16 +5,24 @@
     <div class="menu-items">
         <ul>
             @auth <!-- Check if the user is a guest-->
-            <li><a href="{{ route('profile') }}">PROFILE</a></li>
+                <li><a href="{{ route('profile') }}">MY PROFILE</a></li>
+                @if (Auth::user()->role == 'company')
+                    <li><a href="{{ route('students') }}">STUDENTS</a></li>
+                    <li><a href="{{ route('likes') }}">SAVED STUDENTS</a></li>
+                @else
+                    <li><a href="#">COMPANIES</a></li>
+                    <li><a href="{{ route('likes') }}">SAVED COMPANIES</a></li>
+                @endif
+
             @endauth
-            <li><a href="#">EVENT</a></li>
-            <li><a href="#">EXPLORE</a></li>
+
+            <li><a href="#">EVENTS</a></li>
         </ul>
         @auth
-        <a href="{{ route('logout') }}" class="button">LOG OUT</a>
+            <a href="{{ route('logout') }}" class="button">LOG OUT</a>
         @else
-        <a href="{{ route('register') }}" class="button">SIGN UP</a>
-        <a href="{{ route('login') }}" class="button">LOG IN</a>
+            <a href="{{ route('register') }}" class="button">SIGN UP</a>
+            <a href="{{ route('login') }}" class="button">LOG IN</a>
         @endauth
     </div>
 </div>
