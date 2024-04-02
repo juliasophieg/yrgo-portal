35154ff49d958918432312030150c11119852fe5
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-02 16:58:36 +0200

    styled login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,14 +7,34 @@
 @endsection
 
 @section('content')
-    <form action="/login" method="POST">
-        @csrf
-        <label for="email">Email</label>
-        <input name="email" id="email" type="email">
-        <label for="password">Password</label>
-        <input name="password" id="password" type="password">
-        <button>Login</button>
-    </form>
+    <div class="main">
+        <div class="login-page">
+            <div class="text-top">
+                <div class="text">Logga in</div>
+                <div class="line"></div>
+            </div>
+
+            <div class="form">
+                <form action="/login" method="POST">
+                    @csrf
+                    <div class="inputs">
+                        <div class="input"> <label for="email">E-mail</label>
+                            <input name="email" id="email" type="email" placeholder="ex. Email@email.com" required>
+                        </div>
+                        <div class="input"> <label for="password">Lösenord</label>
+                            <input name="password" id="password" type="password" placeholder="Ditt Lösenord" required>
+                        </div>
+                    </div>
+                    <button>Logga in</button>
+                </form>
+                <div class="bottom-text">
+                    <div class="sub-text">Har du inte ett konto?</div>
+                    <div class="register"><a href="{{ route('register') }}">Skapa Konto</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     @include('components.error')
 @endsection
