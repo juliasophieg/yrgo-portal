6f3397cb52d37a89fbc85c89c18024b1d7c060ce
Julia <jsgoransson@gmail.com>
2024-04-07 16:09:10 +0200

    menu styling and animation

--- public/js/menu.js
+++
@@ -1,7 +0,0 @@
-//Show and hide menu items
-const menuTitle = document.querySelector(".menu-title");
-const menuItems = document.querySelector(".menu-items");
-
-menuTitle.addEventListener("click", function () {
-    menuItems.classList.toggle("active");
-});
