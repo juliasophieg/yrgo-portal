89d840a08205ea104d832a622f81d0af15651d97
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-14 19:41:31 +0200

    styled login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,27 +10,30 @@
     <div class="main">
         @include('components.error')
         <div class="login-page">
-            <div class="text-top">
-                <div class="text">Logga in</div>
-                <div class="line"></div>
-            </div>
+            <div class="width-container">
+                <div class="text-top">
+                    <div class="text">Logga in</div>
+                    <div class="line"></div>
+                </div>
 
-            <div class="form">
-                <form action="/login" method="POST">
-                    @csrf
-                    <div class="inputs">
-                        <div class="input"> <label for="email">E-mail</label>
-                            <input name="email" id="email" type="email" placeholder="ex. Email@email.com" required>
+                <div class="form">
+                    <form action="/login" method="POST">
+                        @csrf
+                        <div class="inputs">
+                            <div class="input"> <label for="email">E-mail</label>
+                                <input name="email" id="email" type="email" placeholder="ex. Email@email.com"
+                                    required>
+                            </div>
+                            <div class="input"> <label for="password">Lösenord</label>
+                                <input name="password" id="password" type="password" placeholder="Ditt Lösenord" required>
+                            </div>
                         </div>
-                        <div class="input"> <label for="password">Lösenord</label>
-                            <input name="password" id="password" type="password" placeholder="Ditt Lösenord" required>
-                        </div>
+                        <button>Logga in</button>
+                    </form>
+                    <div class="bottom-text">
+                        <div class="sub-text">Har du inte ett konto?</div>
+                        <div class="register"><a href="{{ route('register') }}">Skapa Konto</a></div>
                     </div>
-                    <button>Logga in</button>
-                </form>
-                <div class="bottom-text">
-                    <div class="sub-text">Har du inte ett konto?</div>
-                    <div class="register"><a href="{{ route('register') }}">Skapa Konto</a></div>
                 </div>
             </div>
         </div>
