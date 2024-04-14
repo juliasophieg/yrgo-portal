3dabddca54e5896d418db359a66607b7d76ac0e7
Julia <jsgoransson@gmail.com>
2024-04-14 17:07:14 +0200

    added divider

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,38 +3,38 @@
 @section('title', '- Login')
 
 @section('menu')
-    @include('components.menu')
+@include('components.menu')
 @endsection
 
 @section('content')
-    <div class="main">
-        @include('components.error')
-        <div class="login-page">
-            <div class="text-top">
-                <div class="text">Logga in</div>
-                <div class="line"></div>
-            </div>
+<div class="main">
+    @include('components.error')
+    <div class="login-page">
+        <div class="text-top">
+            <div class="text">Logga in</div>
+            <div class="line"></div>
+        </div>
 
-            <div class="form">
-                <form action="/login" method="POST">
-                    @csrf
-                    <div class="inputs">
-                        <div class="input"> <label for="email">E-mail</label>
-                            <input name="email" id="email" type="email" placeholder="ex. Email@email.com" required>
-                        </div>
-                        <div class="input"> <label for="password">Lösenord</label>
-                            <input name="password" id="password" type="password" placeholder="Ditt Lösenord" required>
-                        </div>
+        <div class="form">
+            <form action="/login" method="POST">
+                @csrf
+                <div class="inputs">
+                    <div class="input"> <label for="email">E-mail</label>
+                        <input name="email" id="email" type="email" placeholder="ex. Email@email.com" required>
                     </div>
-                    <button>Logga in</button>
-                </form>
-                <div class="bottom-text">
-                    <div class="sub-text">Har du inte ett konto?</div>
-                    <div class="register"><a href="{{ route('register') }}">Skapa Konto</a></div>
+                    <div class="input"> <label for="password">Lösenord</label>
+                        <input name="password" id="password" type="password" placeholder="Ditt Lösenord" required>
+                    </div>
                 </div>
+                <button>Logga in</button>
+            </form>
+            <div class="bottom-text">
+                <div class="sub-text">Har du inte ett konto?</div>
+                <div class="register"><a href="{{ route('gdpr-consent') }}">Skapa Konto</a></div>
             </div>
         </div>
     </div>
+</div>
 
 
 
