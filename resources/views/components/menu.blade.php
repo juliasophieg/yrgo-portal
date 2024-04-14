b0bd5fefaaf7589a85d5c39d5469d41bba7e609c
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-14 21:06:10 +0200

    styled register page

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,51 +1,55 @@
 <div class="menu">
     <div class="navbar">
-        @if (request()->is('profile') || request()->is('students/*') || request()->is('companies/*'))
-            <a href="{{ URL::previous() }}" class="logo">
-                <img src="{{ asset('images/icons/chevron-left.svg') }}" alt="Logo">
+        <div class="width-container">
+            @if (request()->is('profile') || request()->is('students/*') || request()->is('companies/*'))
+                <a href="{{ URL::previous() }}" class="logo">
+                    <img src="{{ asset('images/icons/chevron-left.svg') }}" alt="Logo">
+                </a>
+            @endif
+            <a href="/" class="logo">
+                <img src="{{ asset('images/logo/logo.svg') }}" alt="Logo">
             </a>
-        @endif
-        <a href="/" class="logo">
-            <img src="{{ asset('images/logo/logo.svg') }}" alt="Logo">
-        </a>
-        <div class="button-menu">
-            @guest <!-- Display login button if user is guest-->
-                <a href="{{ route('login') }}" class="button button-secondary-transparent">LOGGA IN</a>
-            @endguest
-            <div class="hamburger" onclick="toggleMenu()">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+            <div class="button-menu">
+                @guest <!-- Display login button if user is guest-->
+                    <a href="{{ route('login') }}" class="button button-secondary-transparent">LOGGA IN</a>
+                @endguest
+                <div class="hamburger" onclick="toggleMenu()">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
         </div>
     </div>
     <div class="menu-content">
         @auth <!-- Check if the user is logged in-->
 
-        <ul>
-            <li><a href="{{ route('profile') }}">Profil</a></li>
-            @if (Auth::user()->role == 'company')<!-- If logged in user is company -->
-            <li><a href="{{ route('likes') }}">Sparade studenter</a></li>
-            <li><a href="{{ route('students') }}">Sök studenter</a></li>
-            @else <!-- If logged in user is student -->
-            <li><a href="{{ route('likes') }}">Sparade företag</a></li>
-            <li><a href="{{ route('companies') }}">Sök företag</a></li>
-            @endif
-            <li><a href="/">Eventet</a></li>
-        </ul>
-        <div class="divider"></div>
-        <a href="{{ route('logout') }}" class="button button-secondary-transparent">LOGGA UT</a>
-
-        @else <!-- If user is guest -->
-        <ul>
-            <li><a href="/#event-page">Eventet</a></li>
-        </ul>
-        <div class="divider"></div>
-        <div class="button-group">
-            <a href="{{ route('gdpr-consent') }}" class="button button-primary-blue">SKAPA KONTO</a>
-            <a href="{{ route('login') }}" class="button button-secondary-transparent">LOGGA IN</a>
-        </div>
+            <ul>
+                <li><a href="{{ route('profile') }}">Profil</a></li>
+                @if (Auth::user()->role == 'company')
+                    <!-- If logged in user is company -->
+                    <li><a href="{{ route('likes') }}">Sparade studenter</a></li>
+                    <li><a href="{{ route('students') }}">Sök studenter</a></li>
+                @else
+                    <!-- If logged in user is student -->
+                    <li><a href="{{ route('likes') }}">Sparade företag</a></li>
+                    <li><a href="{{ route('companies') }}">Sök företag</a></li>
+                @endif
+                <li><a href="/">Eventet</a></li>
+            </ul>
+            <div class="divider"></div>
+            <a href="{{ route('logout') }}" class="button button-secondary-transparent">LOGGA UT</a>
+        @else
+            <!-- If user is guest -->
+            <ul>
+                <li><a href="/#event-page">Eventet</a></li>
+            </ul>
+            <div class="divider"></div>
+            <div class="button-group">
+                <a href="{{ route('gdpr-consent') }}" class="button button-primary-blue">SKAPA KONTO</a>
+                <a href="{{ route('login') }}" class="button button-secondary-transparent">LOGGA IN</a>
+            </div>
         @endauth
     </div>
 </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,67 +10,70 @@
     <div class="main">
         @include('components.error')
         <div class="register-page">
-            <div class="first-section">
-                <div class="header">
-                    <div class="numbers">
-                        <div class="number-1 active">1</div>
-                        <div class="number-2">2</div>
-                        <div class="number-3">3</div>
+            <div class="width-container">
+                <div class="first-section">
+                    <div class="header">
+                        <div class="numbers">
+                            <div class="number-1 active">1</div>
+                            <div class="number-2">2</div>
+                            <div class="number-3">3</div>
 
-                    </div>
-                    <div class="bottom-header">
-                        <div class="sign-up">
-                            <div class="title">Skapa Konto</div>
-                            <div class="line"></div>
                         </div>
-                        <div class="subtitle">Vem är du? </div>
+                        <div class="bottom-header">
+                            <div class="sign-up">
+                                <div class="title">Skapa Konto</div>
+                                <div class="line"></div>
+                            </div>
+                            <div class="subtitle">Vem är du? </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="choices">
-                    <div class="option-buttons">
-                        <button type="button" id="studentButton" value="student">Student</button>
-                        <button type="button" id="companyButton" value="company">Företag</button>
+                    <div class="choices">
+                        <div class="option-buttons">
+                            <button type="button" id="studentButton" value="student">Student</button>
+                            <button type="button" id="companyButton" value="company">Företag</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="form-section" style="display: none;">
-                <div class="header">
-                    <div class="numbers">
-                        <div class="number-1 active">1</div>
-                        <div class="number-2 active">2</div>
-                        <div class="number-3">3</div>
+                <div class="form-section" style="display: none;">
+                    <div class="header">
+                        <div class="numbers">
+                            <div class="number-1 active">1</div>
+                            <div class="number-2 active">2</div>
+                            <div class="number-3">3</div>
+                        </div>
+                        <div class="form-header">
+                            <div class="form-title">Title</div>
+                            <div class="form-subtitle">Skapa Konto</div>
+                        </div>
                     </div>
-                    <div class="form-header">
-                        <div class="form-title">Title</div>
-                        <div class="form-subtitle">Skapa Konto</div>
-                    </div>
+                    <form id="registrationForm" action="/register" method="POST">
+                        @csrf
+                        <div class="form-content">
+                            <div class="input">
+                                <label for="name" class="name">Namn</label>
+                                <input id="name" name="name" type="name" required
+                                    placeholder="Förnamn Efternamn">
+                            </div>
+                            <div class="input">
+                                <label for="email">Email (syns i profilen)</label>
+                                <input id="email" name="email" type="email" required
+                                    placeholder="ex. name.namesson@mail.com">
+                            </div>
+                            <div class="input">
+                                <label for="password">Lösenord</label>
+                                <input id="password" name="password" type="password" required
+                                    placeholder="Ditt lösenord (minst 8 karaktärer)">
+                            </div>
+                            <input type="hidden" name="role" id="role">
+                            <button type="submit">Skapa Konto</button>
+                        </div>
+                        <div class="bottom-text">
+                            <div class="sub-text">Har du redan ett konto?</div>
+                            <div class="login"><a href="{{ route('login') }}">Logga in</a></div>
+                        </div>
+                    </form>
                 </div>
-                <form id="registrationForm" action="/register" method="POST">
-                    @csrf
-                    <div class="form-content">
-                        <div class="input">
-                            <label for="name" class="name">Namn</label>
-                            <input id="name" name="name" type="name" required placeholder="Förnamn Efternamn">
-                        </div>
-                        <div class="input">
-                            <label for="email">Email (syns i profilen)</label>
-                            <input id="email" name="email" type="email" required
-                                placeholder="ex. name.namesson@mail.com">
-                        </div>
-                        <div class="input">
-                            <label for="password">Lösenord</label>
-                            <input id="password" name="password" type="password" required
-                                placeholder="Ditt lösenord (minst 8 karaktärer)">
-                        </div>
-                        <input type="hidden" name="role" id="role">
-                        <button type="submit">Skapa Konto</button>
-                    </div>
-                    <div class="bottom-text">
-                        <div class="sub-text">Har du redan ett konto?</div>
-                        <div class="login"><a href="{{ route('login') }}">Logga in</a></div>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
