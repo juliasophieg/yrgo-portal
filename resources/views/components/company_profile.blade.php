f9ae9d717be7491cb63b9dc776f4989fa5c28050
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-15 22:15:52 +0200

    added styling for desktop CompaniesProfile

--- resources/views/components/company_profile.blade.php
+++ resources/views/components/company_profile.blade.php
@@ -2,71 +2,80 @@
     <div class="profile-header">
         <div class="profile-img">
             @if ($user->profile_picture == null)
-            <img src="/images/profiles/default_image_company.png" alt="">
+                <img src="/images/profiles/default_image_company.png" alt="">
             @else
-            <img src="/storage/{{ $user->profile_picture }}" alt="">
+                <img src="/storage/{{ $user->profile_picture }}" alt="">
             @endif
         </div>
-        <div class="profile-info">
-            <div class="info">
-                <h1 class="title-2">{{ $user->name }}</h1>
-                <p class="subtitle">{{ $extraInfo->location }}</p>
+        <div class="profile-info-desktop-wrapper">
+            <div class="profile-info">
+                <div class="info">
+                    <h1 class="title-2">{{ $user->name }}</h1>
+                    <p class="subtitle">{{ $extraInfo->location }}</p>
+                </div>
             </div>
             <!-- Display "Redigera" if the logged-in user's ID matches the profile ID -->
             @if ($user->id === Auth::id())
-            <a href="{{ route('edit-profile') }}">Ändra profil</a>
+                <a href="{{ route('edit-profile') }}">Ändra profil</a>
             @endif
         </div>
         <!-- Display save-button if the logged-in user's ID does not match the profile ID -->
         @if ($user->id !== Auth::id())
-        <div class="save">
-            <livewire:like-button :userId="$user->id" />
-        </div>
+            <div class="save">
+                <livewire:like-button :userId="$user->id" />
+            </div>
         @endif
     </div>
     <div class="profile-main">
-        <div class="main-section">
-            <h2 class="title-4">Om oss</h2>
-            <p>{{ $user->description}}</p>
-        </div>
-        <div class="divider"></div>
-        <div class="main-section">
-            <h2 class="title-4">Vad vi jobbar med</h2>
-            <div class="technologies">
-                @foreach($user->technologies as $technology)
-                <div class="technology">{{ $technology->name }}</div>
-                @endforeach
+        <div class="desktop-selection">
+            <div class="main-section">
+                <h2 class="title-4">Om oss</h2>
+                <p>{{ $user->description }}</p>
             </div>
+            <div class="divider"></div>
+            <div class="main-section">
+                <h2 class="title-4">Vad vi jobbar med</h2>
+                <div class="technologies">
+                    @foreach ($user->technologies as $technology)
+                        <div class="technology">{{ $technology->name }}</div>
+                    @endforeach
+                </div>
+            </div>
+
+            <div class="divider"></div>
         </div>
-        <div class="divider"></div>
-        <div class="main-section">
+        <div class="main-section contact">
             <h2 class="title-4">Kontakt</h2>
             <ul>
                 @php
-                $contactInfo = [
-                ['value' => $user->phone, 'label' => null, 'icon' => '/images/icons/phone.svg'],
-                ['value' => $user->email, 'label' => null, 'icon' => '/images/icons/mail.svg'],
-                ['value' => $user->website, 'label' => 'Website', 'icon' => '/images/icons/website.svg'],
-                ['value' => $user->linkedin, 'label' => $user->name, 'icon' => '/images/icons/linkedin.svg'],
-                ['value' => $user->facebook, 'label' => '@' . $user->name, 'icon' => '/images/icons/instagram.svg'],
-                ];
+                    $contactInfo = [
+                        ['value' => $user->phone, 'label' => null, 'icon' => '/images/icons/phone.svg'],
+                        ['value' => $user->email, 'label' => null, 'icon' => '/images/icons/mail.svg'],
+                        ['value' => $user->website, 'label' => 'Website', 'icon' => '/images/icons/website.svg'],
+                        ['value' => $user->linkedin, 'label' => $user->name, 'icon' => '/images/icons/linkedin.svg'],
+                        [
+                            'value' => $user->facebook,
+                            'label' => '@' . $user->name,
+                            'icon' => '/images/icons/instagram.svg',
+                        ],
+                    ];
                 @endphp
 
-                @foreach($contactInfo as $info)
-                @if($info['value'])
-                <li>
-                    @if($info['icon'])
-                    <div class="contact-icon"><img src="{{ $info['icon'] }}" alt=""></div>
-                    @else
-                    <div class="contact-icon"></div>
+                @foreach ($contactInfo as $info)
+                    @if ($info['value'])
+                        <li>
+                            @if ($info['icon'])
+                                <div class="contact-icon"><img src="{{ $info['icon'] }}" alt=""></div>
+                            @else
+                                <div class="contact-icon"></div>
+                            @endif
+                            @if ($info['label'])
+                                <a href="{{ $info['value'] }}">{{ $info['label'] }}</a>
+                            @else
+                                {{ $info['value'] }}
+                            @endif
+                        </li>
                     @endif
-                    @if($info['label'])
-                    <a href="{{ $info['value'] }}">{{ $info['label'] }}</a>
-                    @else
-                    {{ $info['value'] }}
-                    @endif
-                </li>
-                @endif
                 @endforeach
             </ul>
         </div>
