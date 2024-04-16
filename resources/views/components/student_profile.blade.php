f42b8999b185f1c27f7127b3a3d0592ea07385cb
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-16 07:30:30 +0200

    added desktop styling for student profile

--- resources/views/components/student_profile.blade.php
+++ resources/views/components/student_profile.blade.php
@@ -2,9 +2,9 @@
     <div class="profile-header">
         <div class="profile-img">
             @if ($user->profile_picture == null)
-            <img src="/images/profiles/default_image_user.png" alt="">
+                <img src="/images/profiles/default_image_user.png" alt="">
             @else
-            <img src="/storage/{{ $user->profile_picture }}" alt="">
+                <img src="/storage/{{ $user->profile_picture }}" alt="">
             @endif
         </div>
         <div class="profile-info">
@@ -14,52 +14,58 @@
             </div>
             <!-- Display "Redigera" if the logged-in user's ID matches the profile ID -->
             @if ($user->id === Auth::id())
-            <a href="{{ route('edit-profile') }}">Ändra profil</a>
+                <a href="{{ route('edit-profile') }}">Ändra profil</a>
             @endif
         </div>
         <!-- Display "Spara" if the logged-in user's ID does not match the profile ID -->
         @if ($user->id !== Auth::id())
-        <div class="save">
-            <livewire:like-button :userId="$user->id" />
-        </div>
+            <div class="save">
+                <livewire:like-button :userId="$user->id" />
+            </div>
         @endif
 
     </div>
     <div class="profile-main">
-        <div class="main-section">
-            <h2 class="title-4">Om mig</h2>
-            <p>{{ $user->description}}</p>
-        </div>
-        <div class="divider"></div>
-        <div class="main-section">
-            <h2 class="title-4">Mina kompetenser</h2>
-            <div class="technologies">
-                @foreach($user->technologies as $technology)
-                <div class="technology">{{ $technology->name }}</div>
-                @endforeach
+        <div class="desktop-selection">
+            <div class="main-section">
+                <h2 class="title-4">Om mig</h2>
+                <p>{{ $user->description }}</p>
             </div>
+            <div class="divider"></div>
+            <div class="main-section">
+                <h2 class="title-4">Mina kompetenser</h2>
+                <div class="technologies">
+                    @foreach ($user->technologies as $technology)
+                        <div class="technology">{{ $technology->name }}</div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="divider"></div>
         </div>
-        <div class="divider"></div>
-        <div class="main-section">
+        <div class="main-section contact">
             <h2 class="title-4">Kontakt</h2>
             <ul>
                 @php
-                $contactInfo = [
-                ['value' => $user->phone, 'label' => $user->phone, 'icon' => '/images/icons/phone.svg'],
-                ['value' => $user->email, 'label' => $user->email, 'icon' => '/images/icons/mail.svg'],
-                ['value' => $user->website, 'label' => $user->website, 'icon' => '/images/icons/website.svg'],
-                ['value' => $user->linkedin, 'label' => $user->name, 'icon' => '/images/icons/linkedin.svg'],
-                ['value' => $user->facebook, 'label' => '@' . $user->name, 'icon' => '/images/icons/instagram.svg'],
-                ];
+                    $contactInfo = [
+                        ['value' => $user->phone, 'label' => $user->phone, 'icon' => '/images/icons/phone.svg'],
+                        ['value' => $user->email, 'label' => $user->email, 'icon' => '/images/icons/mail.svg'],
+                        ['value' => $user->website, 'label' => $user->website, 'icon' => '/images/icons/website.svg'],
+                        ['value' => $user->linkedin, 'label' => $user->name, 'icon' => '/images/icons/linkedin.svg'],
+                        [
+                            'value' => $user->facebook,
+                            'label' => '@' . $user->name,
+                            'icon' => '/images/icons/instagram.svg',
+                        ],
+                    ];
                 @endphp
 
-                @foreach($contactInfo as $info)
-                @if($info['value'])
-                <li>
-                    <div class="contact-icon"><img src="{{ $info['icon'] }}" alt=""></div>
-                    <a href="{{ $info['value'] }}">{{ $info['label'] }}</a>
-                </li>
-                @endif
+                @foreach ($contactInfo as $info)
+                    @if ($info['value'])
+                        <li>
+                            <div class="contact-icon"><img src="{{ $info['icon'] }}" alt=""></div>
+                            <a href="{{ $info['value'] }}">{{ $info['label'] }}</a>
+                        </li>
+                    @endif
                 @endforeach
             </ul>
         </div>
