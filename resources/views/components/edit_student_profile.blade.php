0c0bb18d00557a08d9e941cafa9009e57171cfcb
Fanny Karlsson <70578538+Karlsson2@users.noreply.github.com>
2024-04-16 11:59:20 +0200

    final css

--- resources/views/components/edit_student_profile.blade.php
+++ resources/views/components/edit_student_profile.blade.php
@@ -28,15 +28,16 @@
         <!-- ABOUT ME -->
 
         <div class="desktop-selection">
-        <div class="main-section">
-            <h2 class="title-4">Om mig</h2>
-            @if ($user->description == null)
-            <textarea id="description" name="description" rows="4" class="form-control" placeholder="Berätta mer om dig själv för att företaget skall få en uppfattning om vem du är."></textarea>
-            @else
-            <textarea id="description" name="description" rows="4" class="form-control">{{ $user->description}}</textarea>
-            @endif
-        </div>
-        <div class="divider"></div>
+            <div class="main-section">
+                <h2 class="title-4">Om mig</h2>
+                @if ($user->description == null)
+                    <textarea id="description" name="description" rows="4" class="form-control user-description"
+                        placeholder="Berätta mer om dig själv för att företaget skall få en uppfattning om vem du är."></textarea>
+                @else
+                    <textarea id="description" name="description" rows="4" class="form-control user-description">{{ $user->description }}</textarea>
+                @endif
+            </div>
+            <div class="divider"></div>
             <!-- COMPETENCIES -->
             <div class="main-section">
                 <h2 class="title-4">Kompetenser</h2>
@@ -49,20 +50,24 @@
                                 <label>{{ $technology->name }}</label>
                                 <input type="checkbox" name="technologies[]" value="{{ $technology->id }}" checked>
                             </div>
-                    @endforeach
-                </div>
-                <div class="more-technologies">Lägg till fler kompetenser</div>
-                <div class="unselected-technologies">
-                    <!-- Hide unselected technologies intitially -->
-                    @foreach($technologies as $technology)
-                    @if (!$user->technologies->contains($technology))
-                    <div class="technology-checkbox">
-                        <label>{{ $technology->name }}</label>
-                        <input type="checkbox" name="technologies[]" value="{{ $technology->id }}">
+                        @endforeach
+                    </div>
+                    <div class="more-technologies">Lägg till fler kompetenser</div>
+                    <div class="unselected-technologies">
+                        <!-- Hide unselected technologies intitially -->
+                        @foreach ($technologies as $technology)
+                            @if (!$user->technologies->contains($technology))
+                                <div class="technology-checkbox">
+                                    <label>{{ $technology->name }}</label>
+                                    <input type="checkbox" name="technologies[]" value="{{ $technology->id }}">
+                                </div>
+                            @endif
+                        @endforeach
                     </div>
                 </div>
-            </div>
 
+
+            </div>
             <div class="divider"></div>
         </div>
         <!-- CONTACT -->
@@ -70,16 +75,18 @@
             <h2 class="title-4">Kontakt</h2>
             <ul>
                 @foreach ([
-                'phone' => ['label' => 'Telefon', 'type' => 'phone', 'icon' => '/images/icons/phone.svg', 'value' => $user->phone ?? '', 'placeholder' => 'Telefonnummer'],
-                'linkedin' => ['label' => 'LinkedIn', 'type' => 'url', 'icon' => '/images/icons/linkedin.svg', 'value' => $user->linkedin, 'placeholder' => 'LinkedIn-URL'],
-                'facebook' => ['label' => 'instagram', 'type' => 'text', 'icon' => '/images/icons/instagram.svg', 'value' => $user->facebook, 'placeholder' => 'Instagram-namn'],
-                'website' => ['label' => 'Hemsida', 'type' => 'url', 'icon' => '/images/icons/website.svg', 'value' => $user->website ?? '', 'placeholder' => 'www.exempel.se']
-                ] as $field => $attributes)
-                <!-- Input fields-->
-                <li>
-                    <img src="{{ $attributes['icon'] }}" alt="">
-                    <input type="{{ $attributes['type'] }}" class="form-control" id="{{ $field }}" name="{{ $field }}" value="{{ $attributes['value'] }}" placeholder="{{ $attributes['placeholder'] }}">
-                </li>
+        'phone' => ['label' => 'Telefon', 'type' => 'phone', 'icon' => '/images/icons/phone.svg', 'value' => $user->phone ?? '', 'placeholder' => 'Telefonnummer'],
+        'linkedin' => ['label' => 'LinkedIn', 'type' => 'url', 'icon' => '/images/icons/linkedin.svg', 'value' => $user->linkedin, 'placeholder' => 'LinkedIn-URL'],
+        'facebook' => ['label' => 'instagram', 'type' => 'text', 'icon' => '/images/icons/instagram.svg', 'value' => $user->facebook, 'placeholder' => 'Instagram-namn'],
+        'website' => ['label' => 'Hemsida', 'type' => 'url', 'icon' => '/images/icons/website.svg', 'value' => $user->website ?? '', 'placeholder' => 'www.exempel.se'],
+    ] as $field => $attributes)
+                    <!-- Input fields-->
+                    <li>
+                        <img src="{{ $attributes['icon'] }}" alt="">
+                        <input type="{{ $attributes['type'] }}" class="form-control" id="{{ $field }}"
+                            name="{{ $field }}" value="{{ $attributes['value'] }}"
+                            placeholder="{{ $attributes['placeholder'] }}">
+                    </li>
                 @endforeach
             </ul>
         </div>
